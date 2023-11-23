52c2c409c419f39bceb6a3b16fd3607325736052
MohammedKhilel <mohammed358041@gmail.com>
2023-11-23 22:43:37 +0200

    table is colors

--- demo.py
+++ demo.py
@@ -33,58 +33,65 @@ class CustomTable(tk.Frame):
 
     def insert_row(self, data):
         self.my_game.insert(parent='',index='end',iid=self.rows,text='',
-                            values=(data[0], data[1]))
+                            values=(data[0], data[1]), tags=(str(self.rows),))
         self.rows += 1
 
-    # Add a method to edit a specific item in the table
+    
     def edit_item(self, row, column, value):
-        # Use the set method of the treeview object to change the value of the item
+        
         self.my_game.set(item=row, column=column, value=value)
     
     def delete_row(self, row):
-        
-        # Use the delete method of the treeview object to remove the item with the given index
+
         self.my_game.delete(row)
-        
         for i in range(row, self.rows - 1):
-            
             self.my_game.move(i+1, '', i)
-        
         self.rows -= 1
 
     def delete_all(self):
 
-        # Use the delete method of the treeview object to remove the item with the given index
         for i in range(self.rows):
             self.my_game.delete(i)
             self.rows -= 1
 
-        # for i in range(row, self.rows - 1):
-        #     self.my_game.move(i + 1, '', i)
-
     def get_item(self, row, column):
-        # Use the item method of the treeview object to get the value of the item
+       
         return self.my_game.item(row, 'values')[column]
-    
-    
-    
-        
 
-'''
-def func():
-    table.edit_item(row=0 , column=0,value="gg")
+    def change_row_color(self, row, color):
+        """
+        Changes the background color of a row in the table.
+        """
+        self.my_game.tag_configure(row, background=color)
 
+    def set_background_color(self, color):
+        self.my_game.configure(style='Custom.Treeview')
+        self.style = ttk.Style()
+        self.style.configure('Custom.Treeview', background=color)
+
+    def reset_All_Rows(self):
+        for i in range(self.rows):
+            self.my_game.tag_configure(i, background="white")
+        self.set_background_color('white')
+  
+
+def func1():
+    table.change_row_color(row=4,color='red')
+def func2():
+    table.reset_All_Rows()
 
 root = tk.Tk()
 root.geometry("500x500")
 root.title('PythonGuides')
 root['bg'] = '#ffffff'
 
+
 table = CustomTable(root, height=11, width1=80,width2=80)
 table.place(x=50,y=30)
-
-btn1 = RoundedButton(root, text="fetch 1", border_radius=10, padding=20, command=func, color="#2bc8c8")          
+btn1 = RoundedButton(root, text="fetch 1", border_radius=10, padding=20, command=func1, color="#172f5f")          
 btn1.place(x=50,y=300)
+btn2 = RoundedButton(root, text="fetch 2", border_radius=10, padding=20, command=func2, color="#172f5f")          
+btn2.place(x=200,y=300)
 table.insert_row((0,0))
 table.insert_row((1,1))
 table.insert_row((2,2))
@@ -105,8 +112,15 @@ table.insert_row((6,6))
 table.insert_row((7,7))
 table.insert_row((8,8))
 table.insert_row((9,9))
+table.change_row_color(row=0,color='red')
+table.change_row_color(row=4,color='red')
+table.change_row_color(row=6,color='red')
+table.change_row_color(row=7,color='red')
+table.change_row_color(row=9,color='red')
+table.change_row_color(row=10,color='red')
+table.change_row_color(row=12,color='red')
+table.set_background_color('blue')
 # Example of using the edit_item method to change the name of the third row to 'Samurai'
 #table.edit_item(row='2', column='player_name', value='Samurai')
 
 root.mainloop()
-'''
